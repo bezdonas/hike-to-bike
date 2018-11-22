11bb0d43f82f7224584c5040a0da1255155223ef
bezdonas <bezdonas@gmail.com>
2018-11-22 14:50:43 +0300

    More tests for LeafletAdapter (#25)

--- src/MapAdapter/__tests__/LeafletAdapter.spec.js
+++ src/MapAdapter/__tests__/LeafletAdapter.spec.js
@@ -1,4 +1,9 @@
-import { initMap, addMarkerToMap } from '../LeafletAdapter.js';
+import {
+  initMap,
+  addMarkerToMap,
+  addStationToMap,
+  distanceBetweenTwoStations,
+} from '../LeafletAdapter.js';
 
 describe('Leaflet', () => {
   const mapId = 'mapNodeId';
@@ -53,6 +58,41 @@ describe('Leaflet', () => {
         title,
       });
       expect(markerInstance.getElement().title).toEqual(title);
+      expect(mapInstance.hasLayer(markerInstance));
+    });
+  });
+  describe('distanceBetweenTwoStations', () => {
+    it('counts distance correctly', () => {
+      expect(distanceBetweenTwoStations([5, 6], [32, 33])).toEqual(
+        38.18376618407357
+      );
+      expect(distanceBetweenTwoStations([56, 43], [12, 32])).toEqual(
+        45.35416188179427
+      );
+      expect(distanceBetweenTwoStations([13, 22], [13, 55])).toEqual(33);
+    });
+  });
+
+  describe('addStationToMap', () => {
+    it('adds marker with popup', () => {
+      const mapInstance = initMap(mapId, [54.505, -0.09], 13);
+      const markerInstance = addStationToMap(mapInstance, {
+        id: 3005,
+        coordinates: [39.94733, -75.14403],
+        name: '191 S. 2nd St.',
+        totalBikes: 11,
+        fillPercentage: 0.36666666666666664,
+        docks: 2,
+      });
+      expect(markerInstance.getLatLng()).toEqual({
+        lat: 39.94733,
+        lng: -75.14403,
+      });
+      expect(markerInstance.getPopup().getContent()).toEqual(
+        `<h4>191 S. 2nd St.</h4>
+    <b>Bikes:</b> 11<br>
+    <b>Docks:</b> 2`
+      );
       expect(mapInstance.hasLayer(markerInstance));
     });
   });
